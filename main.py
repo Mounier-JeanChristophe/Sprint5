2cb8c80aa345c1f577ce766466191ff6e4b047c9
Mounier-JeanChristophe <81242571+Mounier-JeanChristophe@users.noreply.github.com>
2022-01-14 21:49:04 +0100

    Version finale à l'issue du Sprint n°4

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from os import path
 import os
 from glob import glob
 import sys
+from lxml import etree
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as md
 
 
 #recherche les fichiers dans le dossier passé en parametre (chemin_dossier)
@@ -12,97 +15,132 @@ def find_ext(chemin_dossier, ext):
 
 
 
-def pdf_to_txt(inpute_path, output_path) :
-    cmd = "pdftotext -raw " + inpute_path + " -bbox " + output_path
+def pdf_to_txt(inpute_path) :
+    cmd = "./pdfToText.sh "+inpute_path
     os.system(cmd)
 
 
 def find_paragraph(file_path, titre):
     paragraph = ""
-    maChaine2 = "Abstract"
-    pass1 = ""
-    stop1 = "1"
-    stop2 = "Key" 
-    verif = True
-    
-    memorise = ""
+    linePrecedant = "aa"
     with open(file_path, "r") as file :
         for line in file :
-        	if(verif == False):
-        		break
-        		
-        	elif(line.find(maChaine2) != -1 or line.find("ABSTRACT") != -1):
-        		while line:
-        			line = file.readline()
-        			position = line.find("<word")
-        			str = line[position+76 : ]
-        			position2 = str.find("</word>")
-        			pass1 = str[0 : position2]
-        			#print(pass1)
-        			
-        			if(pass1.find("Keywords:") != -1):
-        				verif = False			
-        				break
-        			
-        			if(pass1.find(stop1) != -1):
-        				memorise = line
-        				line = file.readline()
-        				position = line.find("<word")
-        				str = line[position+76 : ]
-        				position2 = str.find("</word>")
-        				pass1 = str[0 : position2]
-        				if(pass1.find("Introduction") != -1 or pass1.find("INTRODUCTION") != -1 ):
-        					verif = False			
-        					break
-        				else:
-        					position = memorise.find(">")
-        					str = memorise[position+1 : ]
-        					position2 = str.find("</word>")
-        					pass1 = str[0 : position2]
-        					position = line.find(">")
-        					str = line[position+1 : ]
-        					position2 = str.find("</word>")
-        					pass2 = str[0 : position2]
-        					
-        					paragraph = paragraph + pass1 + " " + pass2 + " "
-             		
-        			elif(pass1.find(stop2) != -1):
-        				memorise = line
-        				line = file.readline()
-        				position = line.find("<word")
-        				str = line[position+76 : ]
-        				position2 = str.find("</word>")
-        				pass1 = str[0 : position2]
-        				if(pass1.find("words") != -1):
-        					verif = False			
-        					break
-        				position = memorise.find(">")
-        				str = memorise[position+1 : ]
-        				position2 = str.find("</word>")
-        				pass1 = str[0 : position2]
-        				position = line.find(">")
-        				str = line[position+1 : ]
-        				position2 = str.find("</word>");
-        				pass2 = str[0 : position2]
-        				paragraph = paragraph + pass1 + " " + pass2 + " "
-        			
-        			
-        			
-        			else:
-        				position = line.find(">")
-        				str = line[position+1 : ]
-        				position2 = str.find("</word>")
-        				pass1 = str[0 : position2]
-        				#print(pass1)
-        				paragraph = paragraph + pass1 + " " 
-             	
-       	return paragraph
+            if(line.lower().find(titre) != -1): #or line.find(titre) != -1):   
+                       	
+                if(len(line) <= len(titre)+1):
+                        line = file.readline()
+                        while line == "\n":
+                            line = file.readline()
+                while line:
+                    if(titre == "introduction") :
+                        if((line[0:1] == "2" and linePrecedant.strip()[-1:] == ".") or (line[0:3] == "II." and linePrecedant.strip()[-1:] == ".")):
+                             break   
+
+                    else :
+                        if(line.lower().strip() == "1 introduction" or line.strip() == "I. INTRODUCTION") :
+                             break
+
+                    paragraph = paragraph + line.strip() + " "
+                    linePrecedant = line
+                    line = file.readline()
+
+                if(paragraph != "\n"):
+                    return paragraph
+
+            elif((titre == "Conclusion" and line.find(titre) != -1) or line.find("Conclusions") != -1 or titre.upper() in line):
+
+                if(len(line) <= len(titre)+1):
+                    line = file.readline()
+                    while line == "\n":
+                        line = file.readline()
+
+                while line:
+                    if(("References" in line or "REFERENCES" in line)):
+                        break   
+
+                    paragraph = paragraph + line.strip() + " "
+                    linePrecedant = line
+                    line = file.readline()
+
+                if(paragraph != "\n"):
+                    return paragraph
+
+    return "Not Found\n"
+def find_reference(file_path, titre):
+    paragraph = ""
+    with open(file_path, "r") as file :
+        for line in file :
+            if((line.find(titre) != -1 or line.find("REFERENCES") != -1) and len(line.strip())<=13):            	
+                if(len(line) <= len(titre)+1):
+                        line = file.readline()
+                        while line == "\n":
+                            line = file.readline()
+                while line:
+                    if(titre == "References" or titre.upper() == "REFERENCES") :
+                        if(line is None) :
+                             break
+                    paragraph = paragraph + line.strip() + " "
+                    line = file.readline()
+                if(paragraph != "\n"):
+                    return paragraph
+    return "Not Found\n"
+    
+def find_discussion(file_path, titre):
+    paragraph = ""
+    with open(file_path, "r") as file :
+        for line in file :
+            if(line.find(titre) != -1 or line.find("DISCUSSION") != -1):            	
+                if(len(line) <= len(titre)+1):
+                        line = file.readline()
+                        while line == "\n":
+                            line = file.readline()
+                while line:
+                    if(titre == "Discussion" or titre.upper() == "DISCUSSION") :
+                        if(((line.find("References") != -1 or line.find("REFERENCES") != -1 )and len(line.strip())<=11) or (line.find("Conclusions") != -1 or line.find("CONCLUSIONS") != -1 or line.find("Conclusion") != -1 or line.find("CONCLUSION") != -1)):
+                             break
+                    paragraph = paragraph + line.strip() + " "
+                    line = file.readline()
+                if(paragraph != "\n"):
+                    return paragraph
+    return "Not Found\n"
+
+
+def find_corps(file_path):
+    paragraph = ""
+    linePrecedant = ""
+    check = find_paragraph(file_path,"introduction")
+    with open(file_path, "r") as file :
+        for line in file :
+            if(check == "Not Found\n"):
+            	if(line[0:1] == "1" or line[0:3] == "I."):
+                     while line:
+                         if(line.find("Discussion") != -1 or line.find("DISCUSSION") != -1 or line.find("Conclusions") != -1 or line.find("CONCLUSIONS") != -1 or line.find("Conclusion") != -1 or line.find("CONCLUSION") != -1 or ((line.find("References") != -1 or line.find("REFERENCES") != -1) and len(line.strip())<=13)) :
+                               break
+                         paragraph = paragraph + line.strip() + " "
+                         line = file.readline() 
+                     return paragraph     
+			    
+            	linePrecedant = line
+			    
+            else:
+            	if((line[0:1] == "2" and linePrecedant.strip()[-1:] == ".") or (line[0:3] == "II." and linePrecedant.strip()[-1:] == ".")):
+                     while line:
+                         if(line.find("Discussion") != -1 or line.find("DISCUSSION") != -1 or line.find("Conclusions") != -1 or line.find("CONCLUSIONS") != -1 or line.find("Conclusion") != -1 or line.find("CONCLUSION") != -1 or ((line.find("References") != -1 or line.find("REFERENCES") != -1) and len(line.strip())<=13)) :
+                               break
+                         paragraph = paragraph + line.strip() + " "
+                         line = file.readline() 
+                     return paragraph     
+			    
+            	linePrecedant = line
+		    	
+		    
+            
     return "Not Found\n"
 
 #cree un dossier passe en parametre "dossier" dans le chemin passé en parametre "chemin"
-def create_directory(dossier, chemin):
+def create_directory(dossier):
     directory_name = dossier
-    path = chemin + "/" + directory_name
+    path = directory_name
     if(os.path.isdir(path)):
         os.system("rm -r " + path)                  #supprimer le dossier s'il exit    
     os.system("mkdir " + path)                      #re creer le dossier
@@ -110,17 +148,72 @@ def create_directory(dossier, chemin):
 
 
 
+def parser_file_to_xml(target_path, output_path) :
+
+    article = etree.Element("article")
+    file_name = os.path.basename(target_path).replace(".txt", ".pdf")
+    title = ""
+    writer =""
+    abstract = ""
+
+    preamble = etree.SubElement(article, "preamble")
+    preamble.text = file_name
+
+    file = open(target_path, "r")
+    for i in range(2) :
+        title = title + file.readline().strip('\n').strip() + " "
+    
+    for line in file :
+       if(line.lower().find("abstract") != -1):         
+          break;
+       writer = writer + line.strip('\n').strip()
+        	
+    file.close()
+    
+    
+    
+    
+
+    titre = etree.SubElement(article, "titre")
+    titre.text = title
+    
+    auteur = etree.SubElement(article, "auteur")
+    auteur.text = writer
+    
+    abstract = etree.SubElement(article, "abstract")
+    abstract.text = find_paragraph(target_path, "abstract")
+    
+    introduction = etree.SubElement(article,"introduction")
+    introduction.text = find_paragraph(target_path,"introduction")
+    
+    corps = etree.SubElement(article,"corps")
+    corps.text = find_corps(target_path)
+    
+    discu = etree.SubElement(article, "Discussion")
+    discu.text = find_discussion(target_path, "Discussion")
+    
+    conclusion = etree.SubElement(article, "conclusion")
+    conclusion.text = find_paragraph(target_path, "Conclusion")
+    
+    biblio = etree.SubElement(article, "biblio")
+    biblio.text = find_reference(target_path, "References")
+    
+    
+    
+    xmlstr = ET.tostring(article).decode('utf8')
+    newxml = md.parseString(xmlstr)
+    with open(output_path, "a") as output:
+        output.write(newxml.toprettyxml(indent='\t',newl='\n'))
+
+
+
 
 
 def parser_file_to_txt(filepath, output_path) :
-
-    file_name = os.path.basename(filepath).replace(".html", ".pdf")
+    writer = ""
+    file_name = os.path.basename(filepath).replace(".txt", ".pdf")
     title = ""
     abstract = ""
-    maChaine1 = "<title>"
-    verif = True
-    str2 = ""
-    memorise = ""
     #   -- nom de fichier --
 
     f = open(output_path, "a")
@@ -128,93 +221,100 @@ def parser_file_to_txt(filepath, output_path) :
     f.close()
 
     f = open(filepath, "r")
-    #for i in range(2) :
-     #   title = title + f.readline().strip('\n').strip() + " "
+    for i in range(2) :
+        title = title + f.readline().strip('\n').strip() + " "
     
-    for ligne in f:
-    	if(ligne.find(maChaine1) != -1):
-    		position = ligne.find(maChaine1)
-    		str = ligne[position+7 : ]
-    		position2 = str.find("</title>");
-    		title = str[0 : position2]
-    		break
+    for line in f :
+       if(line.lower().find("abstract") != -1):         
+          break;
+       writer = writer + line.strip('\n').strip()
+        	
     f.close()
-    
-    			
+
+	
     #   -- le titre de fichier --
 
     f = open(output_path, "a")
     f.write("\n\n le titre de ce fichier est  : " + title.rstrip() + "\n")
     f.close()
-    
+    f = open(output_path, "a")
+    f.write("\n\n les auteurs  : " + writer.rstrip() + "\n")
+    f.close()
+
     #   -- abstract --
 
     abstract = find_paragraph(filepath, "abstract")
     f = open(output_path, "a")
     f.write("\n\n abstract : " + abstract)
     f.close()
-
+    
+    introduction = find_paragraph(filepath, "introduction")
+    f = open(output_path, "a")
+    f.write("\n\n introduction : " + introduction)
+    f.close()
+    
+    corps = find_corps(filepath)
+    f = open(output_path, "a")
+    f.write("\n\n corps : " + corps)
+    f.close()
+    
+    
+    discussion = find_discussion(filepath, "Discussion")
+    f = open(output_path, "a")
+    f.write("\n\n discussion : " + discussion)
+    f.close()
+    
+    
+    
+    conclusion = find_paragraph(filepath, "Conclusion")
+    f = open(output_path, "a")
+    f.write("\n\n conclusion : " + conclusion)
+    f.close()
+    
+    
+    reference = find_reference(filepath, "References")
+    f = open(output_path, "a")
+    f.write("\n\n references : " + reference)
+    f.close()
+    
+    
 
 def main():
+
+
     args = sys.argv
-    filepath = args[1]   # on prends l'argument apres la commande python3 (chemin de dossier) si le code est dans le meme dossier des fichier il suffit just de mettre le nom de dossier                
-    dossier_txt = create_directory("File_txt", filepath)
-    dossier_output = create_directory("output", filepath)
+    filepath = args[2]   # on prends l'argument apres la commande python3 (chemin de dossier) si le code est dans le meme dossier des fichier il suffit just de mettre le nom de dossier  
+    choix_option = args[1]
+                  
+    dossier_txt = "TEXT"
+    dossier_output = create_directory("OUTPUT")
+    
     files = find_ext(filepath,"pdf")
-
-    for file in files :
-            #File_txt_path = dossier_txt + "/" + os.path.basename(file).replace(".pdf", ".txt")
-            #output_path = dossier_output + "/" + os.path.basename(file).replace(".pdf", ".txt") 
-            #pdf_to_txt(file, File_txt_path)
-            #parser_file_to_txt(File_txt_path, output_path)
-
-            File_txt_path = dossier_txt  + "/" + os.path.basename(file).replace(".pdf", ".html")
-            output_path =  dossier_output + "/" + os.path.basename(file).replace(".pdf", ".txt")
-            pdf_to_txt(file, File_txt_path)
+    
+    pdf_to_txt(filepath)
+    
+    if(choix_option == "-t"): # extraire les information dans un fichier txt
+    	
+    	for file in files :
+		
+            File_txt_path = dossier_txt + "/" + os.path.basename(file).replace(".pdf", ".txt")
+            output_path = dossier_output + "/" + os.path.basename(file).replace(".pdf", ".txt") 
             parser_file_to_txt(File_txt_path, output_path)
+
+    if(choix_option == "-x"): # extraire les information dans un fichier xml
+    	for file in files :
+            
+            File_txt_path = dossier_txt  + "/" + os.path.basename(file).replace(".pdf", ".txt")
+            
+            output_path =  dossier_output + "/" + os.path.basename(file).replace(".pdf", ".xml")
+            parser_file_to_xml(File_txt_path, output_path)
+            
+    if( choix_option != "-t" and  choix_option != "-x"):
+        print("merci de choisir un entre c'est deux drapeaux -t ou -x")
+        return 0        
             
             
-         
-        
+            
 
 if __name__ == "__main__":
     main()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
